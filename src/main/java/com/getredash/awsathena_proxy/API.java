50ec703594666d19c875086a3f2b0b0be1ed6e16
Waldemar Hummer <whummer@atlassian.com>
2017-02-16 10:48:24 +1100

    add log output

--- src/main/java/com/getredash/awsathena_proxy/API.java
+++ src/main/java/com/getredash/awsathena_proxy/API.java
@@ -6,6 +6,8 @@ import spark.Response;
 
 import static spark.Spark.*;
 
+import org.apache.log4j.Logger;
+
 class QueryRequest {
     String athenaUrl;
     String awsAccessKey;
@@ -15,24 +17,30 @@ class QueryRequest {
 }
 
 public class API {
+
+	private static final Logger LOG = Logger.getLogger(API.class);
+
     public static void main(String[] args) {
         String port = System.getenv("PORT");
         if (port != null) {
             port(Integer.valueOf(port));
         }
 
-        Gson gson = new Gson();
+        Gson gson = new com.google.gson.GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
         post("/query", API::queryRequest, gson::toJson);
         get("/ping", (req, res) -> "PONG");
     }
 
     public static Object queryRequest(Request req, Response res) {
-        Gson gson = new Gson();
+        LOG.info("Received query request: " + req.body());
+        Gson gson = new com.google.gson.GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
         QueryRequest body = gson.fromJson(req.body(), QueryRequest.class);
+        LOG.info("Sending request to Athena");
         Athena athena = new Athena(body.athenaUrl, body.awsAccessKey, body.awsSecretKey, body.s3StagingDir);
 
         try {
             Results results = athena.runQuery(body.query);
+            LOG.info("Received response from Athena");
             return results;
         } catch (AthenaException e) {
             halt(400, e.getMessage());
